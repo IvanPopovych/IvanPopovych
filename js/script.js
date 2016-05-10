ce4bf49900f168ca37253ef4290faf2b70c01838
Ivan <ivanpopovych1988@gmail.com>
2016-05-10 08:50:11 +0000

    final

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function () {
+	$('#form').submit(function (event) {
+		$.ajax({
+			url: "https://formspree.io/ivanpopovych1988@gmail.com",
+			method: "POST",
+			data: {
+				name: username.value,
+				email: email.value,
+				message: message.value
+			},
+			dataType: "json"
+		}).done(function () {
+			$("#username").val("");
+			$("#email").val("");
+			$("#message").val("");
+			$('#container').html('<h1>Message send!</h1>');
+		});
+	
+		event.preventDefault();
+	})
+})
